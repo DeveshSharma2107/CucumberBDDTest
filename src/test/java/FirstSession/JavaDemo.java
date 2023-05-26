f98a5a228d4aa592e22bdb007f1f42fa7bc32e71
DeveshSharma2107 <deveshsharma99999@gmail.com>
2023-05-26 11:23:18 +0530

    modified java demo class

--- src/test/java/FirstSession/JavaDemo.java
+++ src/test/java/FirstSession/JavaDemo.java
@@ -3,7 +3,7 @@ package FirstSession;
 public class JavaDemo {
 	public static void main(String[] args)
 	{
-		System.out.println("Hey i m writing my first java code line=");
+		System.out.println("Hey i m writing my first java code line=====");
 	}
 
 }
--- src/test/java/FirstSession/JavaDemo_1.java
+++ src/test/java/FirstSession/JavaDemo_1.java
@@ -1,8 +1,9 @@
 package FirstSession;
 
-public class JavaDemo {
+public class JavaDemo_1 {
 	public static void main(String[] args)
 	{
+		System.out.println("Hey i m writing my first java code line=");
 		System.out.println("Hey i m writing my first java code line=");
 	}
 
